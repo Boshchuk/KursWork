1dfdbd62e1880165540bd94530648603af377543
Boshchuk <nakedsnake.boschuk@gmail.com>
2016-04-11 17:31:18 +0300

    -layers are added; Dal project added with entity framework

---
+++ TayFactory/ToyFactory.Dal/Material.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ToyFactory.Dal
+{
+    public class Material
+    {
+        [Key]
+        public int MaterialId { get; set; }
+
+        public string Code { get; set; }
+
+        public string Title { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
---
+++ TayFactory/ToyFactory.Dal/ToyFactoryContext.cs
@@ -0,0 +1,9 @@
+﻿using System.Data.Entity;
+
+namespace ToyFactory.Dal
+{
+    public class ToyFactoryContext : DbContext
+    {
+        public DbSet<Material> Materials { get; set; }
+    }
+}
