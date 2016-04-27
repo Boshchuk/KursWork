39e8d72d7c362a3e503f76cf16cae740ec763e2e
Victor Boschuk <nakedsnake.boschuk@gmail.com>
2016-04-27 16:04:50 +0300

    -ToyController draft

--- TayFactory/ToyFactory.Business/ToysController.cs
+++ TayFactory/ToyFactory.Business/ToysController.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using ToyFactory.Dal;
 using ToyFactory.Dal.Repositories.Implementation;
+using ToyFactory.Dal.Repositories.Interfaces;
 
 namespace ToyFactory.Business
 {
@@ -12,18 +13,25 @@ namespace ToyFactory.Business
     {
         private readonly ToyFactoryContext _context;
 
-        private readonly MaterialRepository _materialRepository;
+        private readonly IMaterialRepository _materialRepository;
 
-        private readonly 
+        private readonly IToyRepository _toyRepository;
 
         public ToysController()
         {
             _context = new ToyFactoryContext();
+
+            _materialRepository = new MaterialRepository(_context);
+            _toyRepository = new ToyRepository(_context);
+
         }
 
         public ToysController(ToyFactoryContext context)
         {
             _context = context;
+
+            _materialRepository = new MaterialRepository(_context);
+            _toyRepository = new ToyRepository(_context);
         }
 
 
