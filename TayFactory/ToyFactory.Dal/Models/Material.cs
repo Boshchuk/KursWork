606ffe1c3d737eee2054c3859863e7dd25966b45
pro100gus <vvikkttoorr@gmail.com>
2016-05-13 10:03:59 +0300

    -Добавлены коментарии

--- TayFactory/ToyFactory.Dal/Models/Material.cs
+++ TayFactory/ToyFactory.Dal/Models/Material.cs
@@ -2,15 +2,22 @@ using System.ComponentModel.DataAnnotations;
 
 namespace ToyFactory.Dal.Models
 {
+    /// <summary>
+    /// Класс педставляющий объект материла в базе данных и в прогармм
+    /// </summary>
     public class Material
     {
         [Key]
         public int MaterialId { get; set; }
 
         public string Code { get; set; }
-
+        /// <summary>
+        /// Название материала
+        /// </summary>
         public string Title { get; set; }
-
+        /// <summary>
+        /// 
+        /// </summary>
         public decimal Price { get; set; }
     }
 }
